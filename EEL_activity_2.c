08b39ef7222d7c56a42349cbe7f6d6fdb07d74f4
ishitayelne21 <ishitayelne2025.comp@mmcoe.edu.in>
2025-09-30 16:24:48 +0530

    EEL_activity_2.c

--- EEL_activity_2.c
+++ EEL_activity_2.c
@@ -7,16 +7,16 @@ int main()
     printf("Enter temperature of city on day 1: ");
     scanf("%f", &temp1);
     
-    printf("\nEnter temperature of city on day 2:  ");
+    printf("\nEnter temperature of city on day 2: ");
     scanf("%f", &temp2);
     
-    printf("\nEnter temperature of city on day 3:  ");
+    printf("\nEnter temperature of city on day 3: ");
     scanf("%f", &temp3);
     
-    printf("\nEnter temperature of city on day 4:  ");
+    printf("\nEnter temperature of city on day 4: ");
     scanf("%f", &temp4);
     
-    printf("\nEnter temperature of city on day 5:  ");
+    printf("\nEnter temperature of city on day 5: ");
     scanf("%f", &temp5);
     
     avg_temp = (temp1 + temp2 + temp3 + temp4 + temp5) / 5;
